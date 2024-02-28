84c2b323f70c5ad140dbf042784de5537da987f7
Scott Semtner <ssemtner@ucsd.edu>
2024-02-28 11:38:43 -0800

    fix infinite loop

--- ChatServer.java
+++ ChatServer.java
@@ -88,14 +88,13 @@ class ChatHandler implements URLHandler {
               }
               if (new String(Character.toChars(character)).equals("🥹")) {
                 analysis = " This message has a awwww vibe.";
-              } else {
+              }
               characterIndex += 1;
-               }
             }
             if (numberOfExclamationMarks > 2) {
               analysis += " This message ends forcefully.";
             }
-            matchingMessages += line + analysis + "\n\n";
+	    matchingMessages += line + analysis + "\n\n";
           }
         }
       
